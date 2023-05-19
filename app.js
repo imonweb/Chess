e35af70c3a84dbcffbad4a7967e5714ea434ee08
Imon <contact@imonweb.co.uk>
2023-05-19 17:35:00 +0100

    pieces

--- app.js
+++ app.js
@@ -4,5 +4,72 @@ const infoDisplay = document.querySelector('#info-display');
 const width = 8;
 
 const startPieces = [
-  
+  rook, knight, bishop, queen, king, bishop, knight, rook,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  rook, knight, bishop, queen, king, bishop, knight, rook
 ]
+
+function createBoard() {
+  startPieces.forEach((startPiece, i) => {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    square.innerHTML = startPiece
+    square.firstChild?.setAttribute('draggable', true)
+    square.setAttribute('square-id', i)
+    // square.classList.add('beige')
+    const row = Math.floor((63 - i) / 8 ) + 1
+    if(row % 2 === 0){
+      square.classList.add(i % 2 === 0 ? "beige" : "brown")
+    } else {
+      square.classList.add(i % 2 === 0 ? "brown" : "beige")
+    }
+
+    if ( i <= 15 ){
+      square.firstChild.firstChild.classList.add('black')
+    }
+
+    if ( i >= 40 ){
+      square.firstChild.firstChild.classList.add('white')
+    }
+
+
+    gameBoard.append(square)
+  })
+}
+
+createBoard();
+
+const allSquares = document.querySelectorAll('#gameboard .square')
+
+allSquares.forEach(square => {
+  square.addEventListener('dragstart', dragStart)
+  square.addEventListener('dragover', dragOver)
+  square.addEventListener('drop', dragDrop)
+})
+
+let startPositionId 
+let draggedElement
+
+function dragStart(e){
+  startPositionId = e.target.parentNode.getAttribute('square-id')
+  draggedElement = e.target
+}
+
+function dragOver(e){
+  // e.preventDefault()
+  console.log(e.target)
+}
+
+function dragDrop(){
+  e.stopPropagation()
+  const taken = e.target.classList.contains('piece')
+  console.log(e.target)
+
+  // e.target.parentNode.append(draggedElement)
+  // e.target.remove()
+  // e.target.append(draggedElement)
+}
